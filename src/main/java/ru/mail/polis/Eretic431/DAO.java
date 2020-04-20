3eb59063c478d17c800ab259153f324233b10c68
Eretic431 <abdullapower@outlook.com>
2020-04-20 23:54:40 +0300

    Removed empty line

--- src/main/java/ru/mail/polis/Eretic431/DAO.java
+++ src/main/java/ru/mail/polis/Eretic431/DAO.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Iterators;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.Record;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.*;
 
@@ -13,25 +12,24 @@ public class DAO implements ru.mail.polis.DAO {
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
+    public Iterator<Record> iterator(@NotNull ByteBuffer from) {
         Iterator<Map.Entry<ByteBuffer, ByteBuffer>> iterator = map.tailMap(from).entrySet().iterator();
         return Iterators.transform(iterator,
                 element -> Record.of(Objects.requireNonNull(element).getKey(), element.getValue()));
     }
 
     @Override
-    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
+    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) {
         map.put(key, value);
     }
 
     @Override
-    public void remove(@NotNull ByteBuffer key) throws IOException {
+    public void remove(@NotNull ByteBuffer key) {
         map.remove(key);
     }
 
-
     @Override
-    public void close() throws IOException {
+    public void close() {
         map.clear();
     }
 }
